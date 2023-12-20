8c567f35ae7e0f2a0ccaec6f6cdedf6221d03320
Pamposh <pamposh.dhar@amd.com>
2023-12-19 23:34:55 -0600

    Added vim number section

--- mi300_splinter/set_shell.sh
+++ mi300_splinter/set_shell.sh
@@ -106,3 +106,29 @@ if ! dpkg -l | grep -q "cifs-utils"; then
     sudo apt-get install -y cifs-utils
 fi
 
+##############################################################
+
+
+# Vim editor settings
+
+# Setting line numbers in vim
+line_number_style="set number" #absolute line number style
+
+# Check if the ~/.vimrc file exists
+if [ -e ~/.vimrc ]; then
+  # Check if the line number style is already set
+  if ! grep -q "$line_number_style" ~/.vimrc; then
+    # If not, append the line number configuration to the ~/.vimrc file
+    echo "$line_number_style" >> ~/.vimrc
+    echo "Line numbers set in Vim."
+  else
+    echo "Line numbers are already set in Vim."
+  fi
+else
+  # If the ~/.vimrc file doesn't exist, create it and add the line number configuration
+  echo "$line_number_style" > ~/.vimrc
+  echo "Line numbers set in Vim."
+fi
+
+##############################################################
+
