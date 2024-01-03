0d1ad7af9c4446f25c83e599ec6dc090b10bd69b
Pamposh <pamposh.dhar@amd.com>
2024-01-03 16:54:34 -0600

    Modified set_ubuntu.sh to add vim git curl htop nano

--- mi300_splinter/set_ubuntu.sh
+++ mi300_splinter/set_ubuntu.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Update and upgrade the ubuntu system
+sudo apt update && sudo apt upgrade -y
+
 # # python3.10-venv : needed for creating virtual environments
 sudo apt-get install -y python3.10-venv
 
 # # python3-pip : needed for installing packages with pip
 sudo apt-get install -y python3-pip
 
-
+sudo apt install -y vim git curl 
+sudo apt install -y htop  
+sudo apt install -y nano
