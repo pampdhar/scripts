b1b45d7b2a943f6b304fda8ce9ffa688c43fcaae
Pamposh Dhar <pamposh.dhar@amd.com>
2024-01-08 15:10:21 -0600

    Modified the set_ssh_keys.sh file to add the section for default inbuilt ssh agent being used instead of the keychain application

--- mi300_splinter/set_ssh_keys.sh
+++ mi300_splinter/set_ssh_keys.sh
@@ -24,6 +24,8 @@ add_keychain(){
     if ! grep -q "$keychain_command" ~/.bashrc; then
         # Add keychain configuration to the bashrc file
         echo -e "\n# Start keychain and set up SSH agent\n$keychain_command" >> ~/.bashrc
+        echo 'export SSH_AUTH_SOCK="$SSH_AUTH_SOCK"' >> ~/.bashrc
+        echo 'export SSH_AGENT_PID="$SSH_AGENT_PID"' >> ~/.bashrc
         echo 'export PATH="/usr/bin/keychain:$PATH"' >> ~/.bashrc
 
     fi
@@ -33,27 +35,63 @@ add_keychain(){
 
     # Start keychain and set up SSH agent (suppress output)
     $keychain_command > /dev/null 2>&1
+    export SSH_AUTH_SOCK SSH_AGENT_PID
 
     # Display information and instructions
     echo "Keychain has been installed, configured, and the SSH agent has been started."
     echo "To add your SSH keys automatically in future terminals, no additional action is needed."
 }
 
+# Function to add ssh-key with inbuilt commands - no keychain application
+add_key_default(){
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_ed25519_public
+    ssh-add ~/.ssh/id_ed25519_enterprise
+    ssh-add -l
+
+    # Add SSH agent and ssh-add commands to ~/.bashrc
+    if ! grep -q "ssh-agent" ~/.bashrc; then
+        echo "Adding SSH Agent to ~/.bashrc"
+        # Append commands to the ~/.bashrc file
+        {
+        echo -e "\n# Initialize SSH Agent"
+        echo "eval \"\$(ssh-agent -s)\" > /dev/null 2>&1"
+        echo "ssh-add /home/pampdhar/.ssh/id_ed25519_public > /dev/null 2>&1"
+        echo "ssh-add /home/pampdhar/.ssh/id_ed25519_enterprise > /dev/null 2>&1"
+        } >> ~/.bashrc
+        source ~/.bashrc
+    fi
+}
 
 # Function to add SSH key to GitHub - Uses the GITHUB API
 add_ssh_key_to_github() {
     local title=$1
     local token=$2
     local keyfile=$3
-    curl -X POST -H "Authorization: token $token" -d "{\"title\":\"$title\",\"key\":\"$(cat $keyfile.pub)\"}" $API_URL
+
+    response=$(curl -s -w "\n%{http_code}\n" -X POST -H "Authorization: token $token" -d "{\"title\":\"$title\",\"key\":\"$(cat $keyfile.pub)\"}" $API_URL)
+
+    # Split response and status code
+    response_body=$(echo "$response" | head -n -1)
+    http_status=$(echo "$response" | tail -n1)
+
+    # Check if the HTTP status code is 201 (Created)
+    if [ "$http_status" -eq 201 ]; then
+        echo "SSH key successfully added to GitHub."
+    else
+        echo "Failed to add SSH key to GitHub. HTTP status: $http_status"
+        echo "Response body: $response_body"
+    fi
 }
 
 # Function to generate a new ssh key
 generate_new_ssh_key(){
     # Generate SSH key
     ssh-keygen -t ed25519 -f ~/.ssh/$1 -N ''
-    # Giving user the rwx access to the key
-    sudo chmod u+rwx ~/.ssh/$1.pub
+    # Readable by everyone, writable only by the user access to the public key
+    sudo chmod 644 ~/.ssh/$1.pub
+    # Giving only the user the read and write access to the private key
+    sudo chmod 600 ~/.ssh/$1
 }
 
 source_ssh_config(){
@@ -86,8 +124,10 @@ else
 fi
 
 # Finally Run the keychain function to install and run the keychain
-add_keychain
+#add_keychain
 
+# Add key to agent with default method
+add_key_default
 
 
 
